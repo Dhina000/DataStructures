a2f1ba10d21befa7208fe8ef2e8f7152cf57383f
Dhina Bharathi S <98266088+Dhina000@users.noreply.github.com>
2025-11-14 22:32:40 +0530

    Create 8.6CombinationSum3.cpp

---
+++ Recursion/8.6CombinationSum3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private: 
+    void func(int sum, int last, vector<int> &nums, int k, vector<vector<int>> &ans) {
+        if(sum == 0 && nums.size() == k) {
+            ans.push_back(nums);
+            return; 
+        }
+        if(sum <= 0 || nums.size() > k) return; 
+
+        for(int i = last; i <= 9; i++) {
+            if(i <= sum) {
+                nums.push_back(i); 
+                func(sum - i, i + 1, nums, k, ans); 
+                nums.pop_back(); 
+            } else {
+                break;
+            }
+        }
+    } 
+public:
+    // Function to find all possible combinations of k numbers that add up to n
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans; 
+        vector<int> nums; 
+        // Call the recursive function with initial parameters
+        func(n, 1, nums, k, ans);
+        return ans; 
+    }
+};
